d8f9fa6b0170b19c8f5499f0bbbee8df981c87c8
Jon Finerty <jonathan.finerty@huddle.com>
2014-04-22 16:08:27 +0100

    add optional solution name parameter

--- open.ps1
+++ open.ps1
@@ -1,8 +1,8 @@
-function Open-Solution($path = '.') {
-    $sln = ls $path *.sln -Rec | select -First 1;
+function Open-Solution($solutionName = '*', $path = '.') {
+    $sln = Get-ChildItem $path "$solutionName.sln" -Recurse | Select-Object -First 1;
     if ($sln) {
         Write-Host "Opening " $sln.Name " now ..."
-        start $sln.FullName 
+        start $sln.FullName
     }
 }
 
