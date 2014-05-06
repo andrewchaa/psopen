109de56770d49e879d8208b753837bc96de15d17
andrew.chaa <andrew.chaa@huddle.com>
2014-05-06 16:42:54 +0100

    remember the solution that's once opened

--- PsOpen.ps1
+++ PsOpen.ps1
@@ -1,7 +1,67 @@
-function Open-Solution($solutionName = '*', $path = '.') {
-    $sln = Get-ChildItem $path "$solutionName.sln" -Recurse | Select-Object -First 1;
-    if ($sln) {
-        Write-Host "Opening " $sln.Name " now ..."
-        start $sln.FullName
+$solutions = @{}
+
+$favSolutions = $PSScriptRoot + '\favs.xml'
+if(Test-Path $favSolutions) {
+    $solutions = Import-CliXml $favSolutions 
+}
+
+function Open-Solution($name = '*', $path = '.') {
+    if ($name -ne '*') {
+        handleSolutionName($name)
+        return
+    }
+
+    $sln = Get-ChildItem $path "$name.sln" -Recurse | Select-Object -First 1;
+    startSolution($sln)
+}
+
+function Find-Solutions($path = '.') {
+    $slns = Get-ChildItem $path "*.sln" -Recurse
+
+}
+
+# Many thanks to Jeremy Skinner's posthttp://www.jeremyskinner.co.uk/2010/03/07/using-git-with-windows-powershell/
+function TabExpansion($line, $lastWord) {
+    $LineBlocks = [regex]::Split($line, '[|;]')
+    $lastBlock = $LineBlocks[-1]
+
+    switch -regex ($lastBlock) {
+        'Open-Solution (.*)' { handleTab($lastBlock) }
     }
 }
+
+function handleTab($lastBlock) {
+    $solutions.Keys | sort
+}
+
+function handleSolutionName($name) {
+    $fullName = $solutions.Get_Item($name)
+    if ($fullName) {
+        $sln = Get-Item $fullName
+        startSolution $sln
+    }
+}
+
+function startSolution($sln) {
+    if ($sln) {
+        Write-Host "Opening " $sln.Name " now ..."
+        # start $sln.FullName
+        Write-Host "VS " $sln.FullName
+        
+        rememberSolution($sln)
+
+        return
+    }
+
+    Write-Host "Cannot find the solution file" 
+}
+
+function rememberSolution($sln) {
+    $solutions = Import-CliXml $favSolutions 
+    if (!$solutions.ContainsKey($sln.Name)) {
+        $solutions.Add($sln.Name, $sln.FullName)
+    }
+    
+    $solutions | Export-CliXml -Path $favSolutions
+}
+
