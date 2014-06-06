796d149fd093a9f276c489447a1e3863825e9402
andrew.chaa <andrew.chaa@huddle.com>
2014-06-06 12:06:16 +0100

    fix the problem that posh-git tab expansion was not working together with PsOpen

--- PsOpen.ps1
+++ PsOpen.ps1
@@ -1,50 +1,6 @@
 $solutions = @{}
 
-
-
-$favSolutions = $PSScriptRoot + '\favs.xml'
-if(Test-Path $favSolutions) {
-    $solutions = Import-CliXml $favSolutions 
-}
-
-function Open-Solution($name = '*', $path = '.') {
-    if ($name -ne '*') {
-
-        $sln = handleSolutionName($name)
-        if ($sln) {
-            startSolution($sln)
-            return
-        }
-    }
-
-    $sln = Get-ChildItem $path "$name.sln" -Recurse | Select-Object -First 1;
-    startSolution($sln)
-}
-
-function Find-Solutions($path = '.') {
-    $slns = Get-ChildItem $path "*.sln" -Recurse
-
-}
-
 # Many thanks to Jeremy Skinner's posthttp://www.jeremyskinner.co.uk/2010/03/07/using-git-with-windows-powershell/
-function TabExpansion($line, $lastWord) {
-    $LineBlocks = [regex]::Split($line, '[|;]')
-    $lastBlock = $LineBlocks[-1]
-
-    switch -regex ($lastBlock) {
-        'Open-Solution (.*)' { handleTab($lastBlock) }
-    }
-}
-
-function handleTab($lastBlock) {
-    switch -regex ($lastBlock) {
-        'Open-SOlution (.*)$' {
-            $solutions.Keys | Where { $_ -Like $matches[1] + '*' } | sort
-        }
-    }
-    
-}
-
 function handleSolutionName($name) {
     $fullName = $solutions.Get_Item($name)
     if ($fullName) {
@@ -52,6 +8,18 @@ function handleSolutionName($name) {
     }
 
     return Get-ChildItem $path "$name.sln" -Recurse | Select-Object -First 1;
+}
+
+function rememberSolution($sln) {
+    if (Test-Path $favSolutions) {
+        $solutions = Import-CliXml $favSolutions 
+    }
+
+    if ($solutions -and !$solutions.ContainsKey($sln.Name)) {
+        $solutions.Add($sln.Name, $sln.FullName)
+    }
+    
+    $solutions | Export-CliXml -Path $favSolutions
 }
 
 function startSolution($sln) {
@@ -68,15 +36,53 @@ function startSolution($sln) {
     Write-Host "Cannot find the solution file" 
 }
 
-function rememberSolution($sln) {
-    if (Test-Path $favSolutions) {
-        $solutions = Import-CliXml $favSolutions 
+function Open-Solution($name = '*', $path = '.') {
+    if ($name -ne '*') {
+
+        $sln = handleSolutionName($name)
+        if ($sln) {
+            startSolution($sln)
+            return
+        }
     }
 
-    if ($solutions -and !$solutions.ContainsKey($sln.Name)) {
-        $solutions.Add($sln.Name, $sln.FullName)
+    $sln = Get-ChildItem $path "$name.sln" -Recurse | Select-Object -First 1;
+    startSolution($sln)
+}
+
+function handleTab($lastBlock) {
+    switch -regex ($lastBlock) {
+        'Open-SOlution (.*)$' {
+            $solutions.Keys | Where { $_ -Like $matches[1] + '*' } | sort
+        }
     }
     
-    $solutions | Export-CliXml -Path $favSolutions
 }
+
+function Find-Solutions($path = '.') {
+    $slns = Get-ChildItem $path "*.sln" -Recurse
+
+}
+
+
+$favSolutions = $PSScriptRoot + '\favs.xml'
+if(Test-Path $favSolutions) {
+    $solutions = Import-CliXml $favSolutions 
+}
+
+if (Test-Path Function:\TabExpansion) {
+    Rename-Item Function:\TabExpansion TabExpansionBackupForPsOpen
+}
+
+function TabExpansion($line, $lastWord) {
+    $LineBlocks = [regex]::Split($line, '[|;]')
+    $lastBlock = $LineBlocks[-1]
+
+    switch -regex ($lastBlock) {
+        'Open-Solution (.*)' { handleTab($lastBlock) }
+
+        default { if (Test-Path Function:\TabExpansionBackupForPsOpen) { TabExpansionBackupForPsOpen $line $lastWord } }
+    }
+}
+
 
